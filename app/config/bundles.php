095cfff38fd1d60ad51cdae05d377b5d62ffc17b
at <o.tokarczyk2@gmail.com>
2024-08-29 18:03:07 +0200

    pre rozszerzenia doctrine

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
